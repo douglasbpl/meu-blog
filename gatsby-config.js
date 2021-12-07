2b42a769729644443c419b9fd7a010465a618add
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-12-07 17:44:26 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Meu Portfólio",
   },
-  plugins: ["gatsby-plugin-styled-components"],
-  plugins: [`gatsby-plugin-react-helmet`],
-  plugins: ['gatsby-plugin-netlify'],
-   plugins: [ {
-      resolve: "gatsby-source-graphql",
-      options: {
-                typeName: "alldata",
-                fieldName: "alldata",
-                url: "https://api-us-east-1.graphcms.com/v2/ckwgt0db90jga01xo4hkvaz6s/master",
-      },
+   plugins: ["gatsby-plugin-styled-components",
+  {
+    resolve: "gatsby-source-graphql",
+    options: {
+      typeName: "alldata",
+      fieldName: "alldata",
+      url: "https://api-us-east-1.graphcms.com/v2/ckwgt0db90jga01xo4hkvaz6s/master",
     },
-  ]
-  }
+  },
+],
+}
